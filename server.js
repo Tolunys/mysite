49fea13c07bd165d6736e1fb069aa2dd556d8c9f
Tolunys <fatalmama@gmail.com>
2025-06-08 23:08:37 +0300

    deneme

--- server.js
+++ server.js
@@ -160,6 +160,10 @@ app.put('/api/contact/:id', authenticateToken, async (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('API çalışıyor!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server ${PORT} portunda çalışıyor`);
